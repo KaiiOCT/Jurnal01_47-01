951d448e95301f6b0abb96a923428cb4444a018c
Faris Rizky Ramadhan <faris.7826@gmail.com>
2023-09-27 23:07:45 +0700

    Menambahkan Nama, NIM, Kelas

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,6 +1,8 @@
 public class Jurnal01{
 
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("Nama = Faris Rizky Ramadhan");
+        System.out.println("NIM = 607062330048");
+        System.out.println("Kelas = D3 RPLA 47-01");
     }
 }
